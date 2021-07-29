b36e1b35f6f4b89261c819e7f5ff227235f12afb
HadeerEsam <hadeeresamcomm33@gmail.com>
2021-07-29 15:51:43 +0200

    add user service and implement get user info function

---
+++ GitHub-s-Search-feature/src/app/services/user/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  API_url="https://api.github.com";
+
+  constructor(
+    private http:HttpClient
+  ) { }
+  getUserInfo(userName:string):Observable <any>{
+    return this.http.get(`${this.API_url}/users/${userName}`) ;
+   }
+}
