e6acf53864a93e71c859d81e9ca254c7f1896004
Hadeer Esam <hadeeresamcom33@gmail.com>
2021-07-30 16:04:50 +0200

    add modules of component needed, add routing of search pages

--- GitHub-s-Search-feature/src/app/app-routing.module.ts
+++ GitHub-s-Search-feature/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './pages/search/search.component';
+import { UsersComponent } from './pages/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"search",
+    component:SearchComponent
+  },
+  {
+    path:"search/user",
+    component:UsersComponent
+  },
+  {
+    path:"",
+    redirectTo:"search",
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- GitHub-s-Search-feature/src/app/app.module.ts
+++ GitHub-s-Search-feature/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { SearchComponent } from './pages/search/search.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { UsersComponent } from './pages/users/users.component';
+import {SlideMenuModule} from 'primeng/slidemenu';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {PaginatorModule} from 'primeng/paginator';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    InputTextModule,
+    ButtonModule,
+    FormsModule,
+    SlideMenuModule,
+    BrowserAnimationsModule,
+    PaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
